1573f95b415248740f334446fd3f0de88226146c
NafisMahmudAyon <nafismahmudayon@gmail.com.com>
2024-01-12 16:10:02 +0600

    year change added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// App.js
 import React, { useState } from "react";
 import "./App.css";
 
@@ -19,7 +20,8 @@ const months = [
 
 function App() {
 	const [currentDate, setCurrentDate] = useState(new Date());
-	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+	const [isMonthDropdownOpen, setIsMonthDropdownOpen] = useState(false);
+	const [isYearDropdownOpen, setIsYearDropdownOpen] = useState(false);
 
 	const daysInMonth = (year, month) => {
 		return new Date(year, month + 1, 0).getDate();
@@ -61,15 +63,40 @@ function App() {
 		});
 	};
 
+	const decrementYear = () => {
+		setCurrentDate(
+			(prevDate) => new Date(prevDate.getFullYear() - 1, prevDate.getMonth(), 1)
+		);
+	};
+
+	const incrementYear = () => {
+		setCurrentDate(
+			(prevDate) => new Date(prevDate.getFullYear() + 1, prevDate.getMonth(), 1)
+		);
+	};
+
 	const handleMonthSelection = (selectedMonth) => {
 		setCurrentDate((prevDate) => {
 			return new Date(prevDate.getFullYear(), selectedMonth, 1);
 		});
-		setIsDropdownOpen(false);
+		setIsMonthDropdownOpen(false);
 	};
 
-	const toggleDropdown = () => {
-		setIsDropdownOpen(!isDropdownOpen);
+	const toggleMonthDropdown = () => {
+		setIsMonthDropdownOpen(!isMonthDropdownOpen);
+		setIsYearDropdownOpen(false);
+	};
+
+	const handleYearSelection = (selectedYear) => {
+		setCurrentDate((prevDate) => {
+			return new Date(selectedYear, prevDate.getMonth(), 1);
+		});
+		setIsYearDropdownOpen(false);
+	};
+
+	const toggleYearDropdown = () => {
+		setIsYearDropdownOpen(!isYearDropdownOpen);
+		setIsMonthDropdownOpen(false);
 	};
 
 	const renderCalendar = () => {
@@ -138,23 +165,64 @@ function App() {
 		return calendar;
 	};
 
+	const renderMonthDropdown = () => {
+		return (
+			<div className="month-dropdown">
+				{/* <span onClick={goToPreviousMonth}>&#8592;</span> Left arrow */}
+				{months.map((month, index) => (
+					<div key={month} onClick={() => handleMonthSelection(index)}>
+						{month}
+					</div>
+				))}
+				{/* <span onClick={goToNextMonth}>&#8594;</span> Right arrow */}
+			</div>
+		);
+	};
+
+	const renderYearDropdown = () => {
+		const currentYear = currentDate.getFullYear();
+		const startYear = currentYear - 30;
+		const endYear = currentYear + 50;
+
+		const years = Array.from(
+			{ length: endYear - startYear + 1 },
+			(_, index) => startYear + index
+		);
+
+		return (
+			<div className="year-dropdown">
+				{/* <span onClick={decrementYear}>&#8593;</span> Up arrow */}
+				{years.map((year) => (
+					<div key={year} onClick={() => handleYearSelection(year)}>
+						{year}
+					</div>
+				))}
+				{/* <span onClick={incrementYear}>&#8595;</span> Down arrow */}
+			</div>
+		);
+	};
+
 	return (
 		<div className="calendar-container">
 			<h1>React Calendar App</h1>
 			<div className="header">
 				<button onClick={goToPreviousMonth}>&lt; Prev</button>
-				<span onClick={toggleDropdown} className="month-selector">
-					{months[currentDate.getMonth()]}
-					{isDropdownOpen && (
-						<div className="month-dropdown">
-							{months.map((month, index) => (
-								<div key={index} onClick={() => handleMonthSelection(index)}>
-									{month}
-								</div>
-							))}
-						</div>
-					)}
-				</span>
+				<div>
+					<span onClick={goToPreviousMonth}>&#8592;</span>
+					<span className="month-selector" onClick={toggleMonthDropdown}>
+						{months[currentDate.getMonth()]}
+						{isMonthDropdownOpen && renderMonthDropdown()}
+					</span>
+					<span onClick={goToNextMonth}>&#8594;</span>
+				</div>
+				<div>
+					<span onClick={decrementYear}>&#8593;</span>
+					<span className="year-selector" onClick={toggleYearDropdown}>
+						{currentDate.getFullYear()}
+						{isYearDropdownOpen && renderYearDropdown()}
+					</span>
+					<span onClick={incrementYear}>&#8595;</span>
+				</div>
 				<button onClick={goToNextMonth}>Next &gt;</button>
 			</div>
 			<div className="days-of-week">
@@ -170,4 +238,7 @@ function App() {
 }
 
 export default App;
+
+
+
 
